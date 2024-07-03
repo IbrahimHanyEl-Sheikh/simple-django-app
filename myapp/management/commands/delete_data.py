1a1108a5516c505b5a2003ea20ab57f69c10ae1e
Elsherif, Marwan <Marwan.Elsherif@dell.com>
2024-07-03 15:05:00 +0300

    updateing delete_data cmd to truncate not drop

--- myapp/management/commands/delete_data.py
+++ myapp/management/commands/delete_data.py
@@ -1,41 +1,56 @@
+from django.conf import settings
 from django.core.management.base import BaseCommand
 from django.db import connection
 
 class Command(BaseCommand):
-    help = 'Deletes either a single table or the whole database'
+    help = 'Truncates either a single table or the whole database'
 
     def add_arguments(self, parser):
-        parser.add_argument('--table', type=str, help='Name of the table to delete')
-        parser.add_argument('--all', action='store_true', help='Delete the whole database')
+        parser.add_argument('--table', type=str, help='Name of the table to truncate')
+        parser.add_argument('--all', action='store_true', help='Truncate the whole database')
 
     def handle(self, *args, **options):
         table_name = options.get('table')
-        delete_all = options.get('all')
+        truncate_all = options.get('all')
 
-        if table_name and not delete_all:
-            self.delete_table(table_name)
-        elif delete_all and not table_name:
-            self.delete_database()
+        if table_name and not truncate_all:
+            self.truncate_table(table_name)
+        elif truncate_all and not table_name:
+            self.truncate_database()
         else:
             self.stderr.write(self.style.ERROR('Please specify either --table or --all'))
 
-    def delete_table(self, table_name):
+    def truncate_table(self, table_name):
         with connection.cursor() as cursor:
-            cursor.execute(f'DROP TABLE IF EXISTS {table_name}')
-        self.stdout.write(self.style.SUCCESS(f'Table "{table_name}" deleted successfully'))
+            if connection.vendor == 'sqlite':
+                cursor.execute(f'DELETE FROM {table_name}')
+            else:
+                cursor.execute(f'TRUNCATE TABLE {table_name} RESTART IDENTITY CASCADE')
+        self.stdout.write(self.style.SUCCESS(f'Table "{table_name}" truncated successfully'))
 
-    def delete_database(self):
+    def truncate_database(self):
+        # Standard metadata tables to exclude
+        metadata_tables = [
+            'auth_group','auth_group_permissions','auth_permission','auth_user','auth_user_groups','auth_user_user_permissions',
+            'django_migrations', 'django_content_type', 'django_admin_log',
+            'django_session', 'django_site'
+        ]
+
+        # Add the user table to the exclusion list
+        user_table = settings.AUTH_USER_MODEL.replace('.', '_').lower()
+        metadata_tables.append(user_table)
+
         with connection.cursor() as cursor:
-            table_names = connection.introspection.table_names()
+            if connection.vendor == 'sqlite':
+                cursor.execute('PRAGMA foreign_keys=OFF')
+                for table_name in connection.introspection.table_names():
+                    if table_name not in metadata_tables:
+                        cursor.execute(f'DELETE FROM {table_name}')
+                cursor.execute('PRAGMA foreign_keys=ON')
+            else:
+                for table_name in connection.introspection.table_names():
+                    if table_name not in metadata_tables:
+                        cursor.execute(f'TRUNCATE TABLE {table_name} RESTART IDENTITY CASCADE')
+        self.stdout.write(self.style.SUCCESS('All non-metadata tables truncated successfully'))
 
-            # Disable foreign key checks to avoid errors when dropping tables
-            cursor.execute('PRAGMA foreign_keys=OFF')
 
-            # Drop each table
-            for table_name in table_names:
-                cursor.execute(f'DROP TABLE IF EXISTS {table_name}')
-
-            # Enable foreign key checks
-            cursor.execute('PRAGMA foreign_keys=ON')
-
-        self.stdout.write(self.style.SUCCESS('All tables deleted successfully'))
